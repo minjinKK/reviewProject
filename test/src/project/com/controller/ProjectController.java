3757284f8cba4575ff7075ede7c5ea5f492cd383
1510751 <rainst1@naver.com>
2020-02-14 19:51:10 +0900

    총점순위

--- test/src/project/com/controller/ProjectController.java
+++ test/src/project/com/controller/ProjectController.java
@@ -11,14 +11,16 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import project.com.model.MemberVO;
+import project.com.model.ScoreVO;
 import project.com.service.ProjectService;
+
 
 
 
 /**
  * Servlet implementation class ProjectController
  */
-@WebServlet({"/login.do"})
+@WebServlet({"/index.do","/login.do","/rateTotal.do","/main_rating.do","/rateTaste.do"})
 public class ProjectController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 	private ProjectService service;   
@@ -57,15 +59,46 @@ public class ProjectController extends HttpServlet {
 			System.out.println("login result success");
 			
 			
-			RequestDispatcher rd = request.getRequestDispatcher("main_rating.jsp");
+			RequestDispatcher rd = request.getRequestDispatcher("main_rating.do");
 			rd.forward(request, response);
 			}
 			else {
 				RequestDispatcher rd = request.getRequestDispatcher("index.jsp");
 				rd.forward(request, response);
 			}
+		}else if (sPath.equals("/main_rating.do")) {
+			
+			RequestDispatcher rd = request.getRequestDispatcher("rateTotal.do");
+			rd.forward(request, response);
+		}else if (sPath.equals("/rateTotal.do")) {
+			
+			ArrayList<ScoreVO> list = service.rateTotal();
+			System.out.println("list.size():"+list.size());
+			
+			request.setAttribute("list", list);
+			
+			for(ScoreVO vo:list) {
+				System.out.println(vo.toString());
+			}
+			
+			RequestDispatcher rd = request.getRequestDispatcher("main_rating.jsp");
+			rd.forward(request, response);
+		}else if (sPath.equals("/rateTaste.do")) {
+			
+			ArrayList<ScoreVO> list = service.rateTaste();
+			System.out.println("list.size():"+list.size());
+			
+			request.setAttribute("list", list);
+			
+			for(ScoreVO vo:list) {
+				System.out.println(vo.toString());
+			}
+			
+			RequestDispatcher rd = request.getRequestDispatcher("main_rating.jsp");
+			rd.forward(request, response);
 		}
 	}
+	
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
